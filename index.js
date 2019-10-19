84912ade258dc8940a29f6240c4515fe5c88c9e0
Yarbek <yarbek19951@gmail.com>
2019-10-19 11:18:28 +0500

    add message

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const token = "947744233:AAGo6O4hwcL9DqGe7nXaajNBgphw8Z98v4Q";
 const bot = new TgBot(token,{polling:true});
 
 bot.on("message",msg =>{
-	bot.sendMessage(msg.chat.id,"Hello Yarbek Musurmonov")
+	bot.sendMessage(msg.chat.id,"Salom tentak")
 })
