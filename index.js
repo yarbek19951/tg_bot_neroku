731f2652028ceb06a5321d12b314c7c38f5ba225
Yarbek <yarbek19951@gmail.com>
2019-10-19 11:46:10 +0500

    text

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const TgBot = require("node-telegram-bot-api")
 const token = "947744233:AAGo6O4hwcL9DqGe7nXaajNBgphw8Z98v4Q";
 const bot = new TgBot(token,{polling:true});
 bot.on("message",msg =>{
-	bot.sendMessage(msg.chat.id,"Salom xurmatli "+ msg.text)
+	bot.sendMessage(msg.chat.id,"Salom xurmatli "+msg.text.toString() )
 })
